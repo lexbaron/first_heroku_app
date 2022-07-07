31411b1d7b1f8eeac112c057c4f2485079a2db64
brayan <brayanbaron000@gmail.com>
2022-07-06 20:56:42 -0500

    first heroku app

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const rateLimit = require('express-rate-limit');
+const compression = require('compression');
+const helmet = require('helmet')
+const morgan = require('morgan')
+
+// Routers
+const { usersRouter } = require('./routes/users.routes');
+const { postsRouter } = require('./routes/posts.routes');
+const { commentsRouter } = require('./routes/comments.routes');
+
+// Global err controller
+const { globalErrorHandler } = require('./controllers/error.controller');
+
+// Utils
+const { AppError } = require('./utils/appError.util');
+
+// Init express app
+const app = express();
+
+// Enable incoming JSON
+app.use(express.json());
+
+// Limit the number of requests that can be accepted to our server
+const limiter = rateLimit({
+	max: 10000,
+	windowMs: 60 * 60 * 1000, // 1 hr
+	message: 'Number of requests have been exceeded',
+});
+
+app.use(limiter);
+
+app.use(helmet());
+app.use(compression());
+
+
+if (process.env.NODE_ENV === 'development'){
+	app.use(morgan('dev'));
+}else{
+	app.use(morgan('combined'))
+};
+
+
+// Define endpoints
+app.use('/api/v1/users', usersRouter);
+app.use('/api/v1/posts', postsRouter);
+app.use('/api/v1/comments', commentsRouter);
+
+// Handle incoming unknown routes to the server
+app.all('*', (req, res, next) => {
+	next(
+		new AppError(
+			`${req.method} ${req.originalUrl} not found in this server`,
+			404
+		)
+	);
+});
+
+app.use(globalErrorHandler);
+
+module.exports = { app };
